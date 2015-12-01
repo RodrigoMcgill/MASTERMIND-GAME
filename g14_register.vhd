4fab2bc578a7080e82c25bebf2001821aaa69ab1
Juan-Ca <juan.borges@mail.mcgill.ca>
2015-12-01 13:55:49 -0500

    Updated the register

--- g14_register.vhd
+++ g14_register.vhd
@@ -2,12 +2,23 @@ library ieee;
 use ieee.std_logic_1164.all;
 
 entity g14_register is
-port();
+generic(N :integer := 5);
+port(	data 			: in std_logic_vector(N downto 0);
+		enable, CLK : in std_logic;
+		display 		: out std_logic_vector(N downto 0));
 
 end g14_register;
 
-architecture behavior for g14_register is
+architecture behavior of g14_register is
 
 begin
+process(clk)
+begin
+	if(rising_edge(clk))then
+	  if(enable ='1')then
+		display<=data;
+		end if;
+	end if;
 
+end process;
 end behavior;
